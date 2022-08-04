e79bb169e3b5569b673dae6214019b3a9071c81d
monukmodi <monukr2012@gmail.com>
2022-08-04 06:16:02 +0530

    Project Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './Pages/Home'
+import ExerciseDetail from './Pages/ExerciseDetail'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <Box width ="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
